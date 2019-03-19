388bdc5f54c55fa8a877ddb60ddd6e258fb5a91f
abrar124 <abrar.khurshid.124@gmail.com>
2019-03-19 14:15:31 +0500

    Implement mongoDB for room booking

--- server.js
+++ server.js
@@ -1,24 +1,34 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const { WebhookClient } = require("dialogflow-fulfillment");
 const expressApp = express().use(bodyParser.json());
 
-// mongoose.Promise = global.Promise;
-// const dburi =
-//   "mongodb://abrar:dialogflow124@ds217976.mlab.com:17976/hotel_booking_dialogflow";
-// mongoose.connect(dburi, { useNewUrlParser: true }).catch(err => {
-//   console.log("error occured", err);
-// });
+mongoose.Promise = global.Promise;
+const dburi =
+  "mongodb://abrar:dialogflow124@ds217976.mlab.com:17976/hotel_booking_dialogflow";
+mongoose.connect(dburi, { useNewUrlParser: true }).catch(err => {
+  console.log("error occured", err);
+});
 
-// mongoose.connection.on("connected", () => {
-//   console.log("App is connected with database.");
-// });
+mongoose.connection.on("connected", () => {
+  console.log("App is connected with database.");
+});
 
-// mongoose.connection.on("disconnected", () => {
-//   console.log("App disconnected with database.");
-//   process.exit(1);
-// });
+mongoose.connection.on("disconnected", () => {
+  console.log("App disconnected with database.");
+  process.exit(1);
+});
+
+var userDetail = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    persons: { type: Number, required: true },
+    email: { type: String, required: true }
+  },
+  { collection: "userInfo" }
+);
+var model = new mongoose.model("userInfo", userDetail);
 
 expressApp.post("/webhook", function(request, response, next) {
   const agent = new WebhookClient({ request: request, response: response });
@@ -38,6 +48,26 @@ expressApp.post("/webhook", function(request, response, next) {
     const email = agent.parameters.email;
 
     console.log(name, persons, email);
+
+    var data = {
+      name: name,
+      persons: persons,
+      email: email
+    };
+
+    console.log(data);
+
+    var saveData = new model(data);
+    saveData.save((err, mydata) => {
+      if (err) {
+        console.log(err);
+        agent.add(`Erroe while writing on database`);
+      } else {
+        agent.add(`Thanks! ${name} your request for ${persons} 
+    persons have forwarded we will contact you on ${email}`);
+      }
+    });
+
     agent.add(`Thanks! ${name} your request for ${persons} 
     persons have forwarded we will contact you on ${email}`);
   }
