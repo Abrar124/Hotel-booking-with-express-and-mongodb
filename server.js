04f74dad85f001fc4fbaaec3e8aea48810ddc980
Abrar124 <abrar.khurshid.124@gmail.com>
2019-03-23 00:54:06 +0500

    make it better

--- server.js
+++ server.js
@@ -96,25 +96,26 @@ expressApp.post("/webhook", function (request, response, next) {
 
   function sendMail(agent) {
       const emailToSent = "abrar.khurshid.124@gmail.com";
-    let account =  nodemailer.createTestAccount();
+    // let account =  nodemailer.createTestAccount();
 
-    let transporter = nodemailer.createTransport({
-      host: "smtp.ethereal.email",
-      port: 587,
-      secure: false, // true for 465, false for other ports
-      auth: {
-        user: account.user, // generated ethereal user
-        pass: account.pass // generated ethereal password
-      }
-    });
-
-    // const transporter = nodemailer.createTransport({
-    //   service: "gmail",
+    // let transporter = nodemailer.createTransport({
+    //   host: "smtp.ethereal.email",
+    //   port: 587,
+    //   secure: false, // true for 465, false for other ports
     //   auth: {
-    //     user: "abrar.khurshid.120@gmail.com",
-    //     pass: "12181189012"
+    //     user: account.user, // generated ethereal user
+    //     pass: account.pass // generated ethereal password
     //   }
     // });
+
+    const transporter = nodemailer.createTransport({
+      service: "smtp.gmail.com",
+      auth: {
+        user: "abrar.khurshid.120@gmail.com",
+        pass: "12181189012"
+      }
+    });
+    console.log(112);
 
     var mailOptions = {
       from: "abrar.khurshid.120@gmail.com",
@@ -122,7 +123,7 @@ expressApp.post("/webhook", function (request, response, next) {
       subject: "Dialogflow Mail...",
       text: "I am successfully implement the mail functionality on chatbot"
     };
-
+console.log(emailToSent);
     transporter.sendMail(mailOptions, function (error, info) {
       if (error) {
         console.log(error);
