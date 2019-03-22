d13324991fa7533d51e1ff5d5fc42f9f7f33c73b
Abrar124 <abrar.khurshid.124@gmail.com>
2019-03-23 00:03:43 +0500

    make it better

--- server.js
+++ server.js
@@ -96,25 +96,25 @@ expressApp.post("/webhook", function (request, response, next) {
 
   function sendMail(agent) {
       const emailToSent = "abrar.khurshid.124@gmail.com";
-    // let account =  nodemailer.createTestAccount();
+    let account =  nodemailer.createTestAccount();
 
-    // let transporter = nodemailer.createTransport({
-    //   host: "smtp.ethereal.email",
-    //   port: 587,
-    //   secure: false, // true for 465, false for other ports
-    //   auth: {
-    //     user: account.user, // generated ethereal user
-    //     pass: account.pass // generated ethereal password
-    //   }
-    // });
-
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
+    let transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 587,
+      secure: false, // true for 465, false for other ports
       auth: {
-        user: "abrar.khurshid.120@gmail.com",
-        pass: "12181189012"
+        user: account.user, // generated ethereal user
+        pass: account.pass // generated ethereal password
       }
     });
+
+    // const transporter = nodemailer.createTransport({
+    //   service: "gmail",
+    //   auth: {
+    //     user: "abrar.khurshid.120@gmail.com",
+    //     pass: "12181189012"
+    //   }
+    // });
 
     var mailOptions = {
       from: "abrar.khurshid.120@gmail.com",
